88c1f7fc91e4a4893f75dcacaad497654fc8c4dc
Noah099 <49595302+Noah099@users.noreply.github.com>
2019-04-13 19:39:15 +0100

    Testing

---
+++ Horror Game/Assets/Scripts/Noah Test/PlayerTwo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwo : MonoBehaviour
+{
+
+    public float jumpSpeed = 5f;
+
+    void Jump()
+    {
+        transform.Translate(Vector3.up * jumpSpeed);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+
+
+}
