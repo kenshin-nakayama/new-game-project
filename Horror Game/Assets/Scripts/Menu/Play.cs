2341dcf48e21653c4d0c76521f91c8c48157c974
Noah099 <49595302+Noah099@users.noreply.github.com>
2019-04-13 23:44:14 +0100

    Menu

---
+++ Horror Game/Assets/Scripts/Menu/Play.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Play : MonoBehaviour
+{
+    public void FirstPerson()
+    {
+        Debug.Log("FirstPerson");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ThirdPerson()
+    {
+        Debug.Log("ThirdPerson");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("Has Quit");
+        Application.Quit();
+    }
+}
